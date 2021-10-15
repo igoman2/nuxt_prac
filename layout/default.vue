36a13ad825748a89e3c6f973ba99cbdd36742a57
junho.han <junho.han@ebridge-world.com>
2021-10-16 00:10:25 +0900

    axios

---
+++ layout/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <header>
+      <h1>{{ $route.name }} 페이지asd</h1>
+      asd
+      <NuxtLink to="/"> </NuxtLink>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,85 @@
 <template>
-  <Tutorial/>
+  <div class="app">
+    <main>
+      <div>
+        <input type="text" />
+      </div>
+      <ul>
+        <li
+          v-for="product in products"
+          :key="product.id"
+          @click="moveToDetailPage(product.id)"
+        >
+          <img
+            class="product-image"
+            :src="product.imageUrl"
+            :alt="product.name"
+          />
+          <p>
+            {{ product.name }}
+          </p>
+          <span>
+            {{ product.price }}
+          </span>
+        </li>
+      </ul>
+    </main>
+  </div>
 </template>
 
 <script>
-export default {}
+import axios from 'axios'
+export default {
+  async asyncData() {
+    const resp = await axios.get('http://localhost:3000/products')
+    const products = resp.data.map((item) => {
+      return {
+        ...item,
+        imageUrl: `${item.imageUrl}?random=${Math.random()}`,
+      }
+    })
+
+    return { products }
+  },
+  methods: {
+    moveToDetailPage(id) {
+      console.log(id)
+      this.$router.push(`detail/${id}`)
+    },
+  },
+}
 </script>
+
+<style scoped>
+.flex {
+  display: flex;
+  justify-content: center;
+}
+.item {
+  display: inline-block;
+  width: 400px;
+  height: 300px;
+  text-align: center;
+  margin: 0 0.5rem;
+  cursor: pointer;
+}
+.product-image {
+  width: 400px;
+  height: 250px;
+}
+.app {
+  position: relative;
+}
+.cart-wrapper {
+  position: sticky;
+  float: right;
+  bottom: 50px;
+  right: 50px;
+}
+.cart-wrapper .btn {
+  display: inline-block;
+  height: 40px;
+  font-size: 1rem;
+  font-weight: 500;
+}
+</style>
